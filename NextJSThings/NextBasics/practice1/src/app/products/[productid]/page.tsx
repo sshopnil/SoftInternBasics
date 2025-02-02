18a2c1dcecc55d53d1de3d5badcf7e2232c74c71
Shayanta Shopnil <shopnil710@gmail.com>
2025-02-02 16:02:31 +0600

    routing metadata

--- NextJSThings/NextBasics/practice1/src/app/products/[productid]/page.tsx
+++ NextJSThings/NextBasics/practice1/src/app/products/[productid]/page.tsx
@@ -1,4 +1,18 @@
-export default async function ProductDetails({params,}: {params: Promise<{productid: string}>;}){
+import { Metadata } from "next";
+
+type Props = {
+    params: Promise<{productid:string}>
+}
+
+
+export const generateMetadata = async ({params}:Props):Promise<Metadata>=>{
+    const id = (await params).productid;
+    return{
+        title: `Product ${id}`,
+    };
+}
+
+export default async function ProductDetails({params,}: Props){
     const prodId = (await params).productid;
     // console.log(prodId);
     return <div>

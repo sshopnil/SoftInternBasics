2c2120e94ab160f5acdd7bb23406d3755335acc9
Shayanta Shopnil <shopnil710@gmail.com>
2025-02-02 17:05:04 +0600

    created navlinks

--- NextJSThings/NextBasics/practice1/src/app/page.tsx
+++ NextJSThings/NextBasics/practice1/src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home (){
-    return <h1> Home Page </h1>;
+    return <div>
+        <h1> Home Page </h1>
+        <Link href={'/blog'}>Blog</Link>
+        <Link href={'/products'}>Product Lists</Link>
+        <Link href={'/counter'}>Counter</Link>
+    </div>;
 }

3b986cda87af09933e312161ce04a3c4f935c372
Shayanta Shopnil <shopnil710@gmail.com>
2025-02-03 13:16:26 +0600

    recovering from errors

--- NextJSThings/NextBasics/practice1/src/app/products/[productid]/review/[reviewid]/error.tsx
+++ NextJSThings/NextBasics/practice1/src/app/products/[productid]/review/[reviewid]/error.tsx
@@ -1,5 +1,24 @@
 "use client";
 
-export default function ErrorBoundary({error}:{error: Error}){
-    return <div>{error.message}</div>
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+
+export default function ErrorBoundary({
+    error, reset
+}:{
+    error: Error,
+    reset: ()=> void
+}){
+    const router = useRouter();
+    const reload = ()=>{
+        startTransition(()=>{
+            router.refresh();
+            reset();
+        })
+    }
+    return (<div>
+        {error.message}
+        <button onClick={reload}>Try Again</button>
+    </div>)
 }

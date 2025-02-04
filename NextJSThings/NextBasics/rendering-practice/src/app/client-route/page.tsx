d940f5b5be6ecb43d74c06fc939b469aa828f7c9
Shayanta Shopnil <shopnil710@gmail.com>
2025-02-04 16:15:18 +0600

    added context provider

--- NextJSThings/NextBasics/rendering-practice/src/app/client-route/page.tsx
+++ NextJSThings/NextBasics/rendering-practice/src/app/client-route/page.tsx
@@ -1,8 +1,11 @@
 "use client";
+import { useTheme } from "@/components/theme-provider";
+
 export default function ClientRoutePage(){
+    const theme = useTheme();
     return (
         <div>
-            <h1>This is a Client page</h1>
+            <h1 style={{color: theme.colors.primary}}>This is a Client page</h1>
         </div>
     )
 }

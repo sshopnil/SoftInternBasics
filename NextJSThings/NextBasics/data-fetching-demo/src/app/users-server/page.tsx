4d887daac9ab39635ccaf23befa866eee93f7b0f
Shayanta Shopnil <shopnil710@gmail.com>
2025-02-04 17:55:33 +0600

    sequential posts

--- NextJSThings/NextBasics/data-fetching-demo/src/app/users-server/page.tsx
+++ NextJSThings/NextBasics/data-fetching-demo/src/app/users-server/page.tsx
@@ -8,7 +8,7 @@ type User = {
 }
 
 export default async function UsersServer() {
-
+    await new Promise((resolve)=> setTimeout(resolve, 2000));
     const response = await fetch(
         "https://jsonplaceholder.typicode.com/users"
     );

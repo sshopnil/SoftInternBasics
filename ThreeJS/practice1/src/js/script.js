3171b7a3a57a22aab204a6fb8b0d6f27f549cbe0
Shayanta Shopnil <shopnil710@gmail.com>
2025-02-10 10:03:54 +0600

    model importing

--- ThreeJS/practice1/src/js/script.js
+++ ThreeJS/practice1/src/js/script.js
@@ -1,6 +1,12 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
+import neebula from '../img/nebula.jpg';
+import stars from '../img/stars.jpg';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+
+const monkeyUrl = new URL('../assets/monkey.glb', import.meta.url);
 
 
 const renderer = new THREE.WebGLRenderer();
@@ -134,6 +140,41 @@ scene.add(sLightHelper);
 //to match plane with grid
 plane.rotation.x = -0.5 * Math.PI;
 
+//fog
+scene.fog = new THREE.FogExp2(0xFFFFFF, 0.01); //fade when zoomed out
+// renderer.setClearColor(0xFFEA00);//change background color
+
+//texture
+const textureLoader = new THREE.TextureLoader();
+// scene.background = textureLoader.load(stars);
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([
+    stars,
+    stars,
+    stars,
+    stars,
+    stars,
+    stars
+])
+
+//nebula object
+const nebula_sphere_geo = new THREE.SphereGeometry(5);
+const nebula_sphere_material = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(neebula)
+});
+const nebula_sphere = new THREE.Mesh(nebula_sphere_geo, nebula_sphere_material);
+scene.add(nebula_sphere);
+nebula_sphere.position.set(0, 15, 10);
+
+
+const monkeyLoader = new GLTFLoader();
+monkeyLoader.load(monkeyUrl.href, (gltf)=>{
+    const model = gltf.scene;
+    scene.add(model);
+    model.position.set(12, 4, 10);
+})
+
+
 const gui = new dat.GUI();
 
 const options = {
@@ -142,7 +183,8 @@ const options = {
     speed: 0.01,
     angle: 0.2,
     penumbra: 0,
-    intensity: 1
+    intensity: 1,
+    ambient: 10,
 }
 
 gui.addColor(options, 'sphereColor').onChange((e)=>{
@@ -158,25 +200,51 @@ gui.add(options, 'speed', 0, 0.1);
 gui.add(options, 'angle', 0, 0.1);
 gui.add(options, 'penumbra', 0, 1);
 gui.add(options, 'intensity', 0, 1);
+gui.add(options, 'ambient', 0, 10);
 
 
 let step = 0;
 let speed = 0.01;
 
+const mousePosition = new THREE.Vector2();
+
+window.addEventListener('mousemove', (e)=>{
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
+});
+
+const raycaster = new THREE.Raycaster();
+
+const nebula_sphere_id = nebula_sphere.id;
 
 
 function Animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
-
+    
 //animate the ball bounce
     step += options.speed;
     sphere.position.y = 10 * Math.abs(Math.sin(step));
     
+    ambientLight.intensity = options.ambient;
+
     spotLight.angle = options.angle;
     spotLight.penumbra = options.penumbra;
     spotLight.intensity = options.intensity;
     sLightHelper.update();
+
+
+    raycaster.setFromCamera(mousePosition, camera);
+    const intersectObjects = raycaster.intersectObjects(scene.children);
+
+
+    // console.log(intersectObjects);
+
+    for (let i = 0; i < intersectObjects.length; i++){
+        if(intersectObjects[i].object.id === nebula_sphere_id){
+            intersectObjects[i].object.rotation.y = time/1000;
+        }
+    }
 
     //link the scene with camera
     renderer.render(scene, camera);

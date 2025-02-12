e066c29ae8cf09d126690b6e60180fdfb3c45da4
Shayanta Shopnil <shopnil710@gmail.com>
2025-02-12 17:21:25 +0600

    loading page added

--- Projects/three-adventure/src/app/buy/page.js
+++ Projects/three-adventure/src/app/buy/page.js
@@ -1,10 +1,10 @@
 import { Scene } from "@/components/Scene";
 import ButtonComponent from "@/components/ButtonComponent";
 import MeshComponent from "@/components/MeshComponent";
-
+import { Suspense } from "react";
 export default function Buy (){
-    return(<>
+    return(<Suspense>
     <MeshComponent/>
     <ButtonComponent/>
-    </>);
+    </Suspense>);
 }
